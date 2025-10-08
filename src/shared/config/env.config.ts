dbc5ab8240d57e8510a133c5a566eb2423a1cef1
MES4game <mes4.game@gmail.com>
2025-10-08 17:31:52 +0200

    chore(dev-4): github code owners, PR template and workflows CI/CD

--- src/shared/config/env.config.ts
+++ src/shared/config/env.config.ts
@@ -1,19 +1,18 @@
 import { unknownToBoolean, unknownToString } from "@/shared/utils/common/convert.util";
 import { createConverter, createMapper } from "@/shared/utils/common/mapper.util";
 
-const TMP_ENV = process.env;
-
 interface IEnv {
-    dev : boolean;
-    host: string;
+    dev     : boolean;
+    host    : string;
+    api_url : string;
+    data_url: string;
 }
 
 const mapEnv = createMapper<IEnv>({
-    dev : createConverter(unknownToBoolean, false),
-    host: createConverter(unknownToString, "localhost"),
+    dev     : createConverter(unknownToBoolean, false),
+    host    : createConverter(unknownToString, "127.0.0.1"),
+    api_url : createConverter(unknownToString, "https://127.0.0.1/api"),  // eslint-disable-line @typescript-eslint/naming-convention
+    data_url: createConverter(unknownToString, "https://127.0.0.1/data"),  // eslint-disable-line @typescript-eslint/naming-convention
 });
 
-export const ENV = mapEnv({
-    dev : TMP_ENV.NODE_ENV === "development",
-    host: TMP_ENV.HOST ?? "localhost",
-});
+export const ENV = mapEnv(await (await fetch("/runtime-config.json")).json());
