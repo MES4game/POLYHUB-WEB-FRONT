55d68635c91102b58e7c1b09aec8650c51720fc9
MHoupli <maelhoupline@gmail.com>
2025-10-13 22:59:04 +0200

    style(SCRUM-8-LoginPage-Front): front pour la page de connexion et d'inscription

---
+++ src/ui/components/log/login-form.component.tsx
@@ -0,0 +1,111 @@
+import { Button } from "#/components/ui/button";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+} from "#/components/ui/card";
+import {
+    Field,
+    FieldDescription,
+    FieldGroup,
+    FieldLabel,
+} from "#/components/ui/field";
+import { Input } from "#/components/ui/input";
+import {
+    FC,
+    ReactNode,
+    useEffect,
+} from "react";
+import { Link } from "react-router-dom";
+
+const LoginFormComp: FC = (): ReactNode => {
+    useEffect(() => {
+        console.log("Loaded: LoginFormComp");
+    }, []);
+    
+    useEffect(() => {
+        console.log("Rendered: LoginFormComp");
+    });
+
+    return (
+        <div>
+            <Card>
+                <CardHeader>
+                    <CardTitle>
+                        Connexion
+                    </CardTitle>
+
+                    <CardDescription>
+                        Connectez-vous pour récupérer vos préférences
+                    </CardDescription>
+
+                </CardHeader>
+
+                <CardContent>
+                    <form>
+                        <FieldGroup>
+                            <Field>
+                                <FieldLabel htmlFor="pseudo">
+                                    Identifiant
+                                </FieldLabel>
+
+                                <Input
+                                    id="pseudo"
+                                    type="text"
+                                    required
+                                />
+                            </Field>
+                            
+                            <Field>
+                                <div className="flex items-center">
+                                    <FieldLabel htmlFor="password">
+                                        Mot de passe
+                                    </FieldLabel>
+
+                                    <a
+                                        href="#"
+                                        className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                                    >
+                                        Mot de passe oublié?
+                                    </a>
+                                </div>
+
+                                <Input
+                                    id="password"
+                                    type="password"
+                                    required
+                                />
+                            </Field>
+
+                            <Field>
+                                <Button type="submit">
+                                    Se connecter
+                                </Button>
+
+                                <FieldDescription className="text-center">
+                                    Vous n'avez pas de compte?
+                                    {" "}
+
+                                    <Link to="/register">
+                                        Créer un compte
+                                    </Link>
+                                </FieldDescription>
+
+                                <Link
+                                    to="/"
+                                    className="text-center"
+                                >
+                                    Continuer sans se connecter
+                                </Link>
+                            </Field>
+                        </FieldGroup>
+                    </form>
+                </CardContent>
+            </Card>
+        </div>
+    );
+};
+
+export default LoginFormComp;
