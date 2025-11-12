27401caf85a67750a11c88df2f2035fa28a78db9
MHoupli <maelhoupline@gmail.com>
2025-11-12 01:37:03 +0100

    feat(SCRUM-22-AdminPage): Link front and back in rooms tab

--- src/shared/models/common/location.model.ts
+++ src/shared/models/common/location.model.ts
@@ -1,14 +1,30 @@
-import { unknownToString } from "@/shared/utils/common/convert.util";
+import { unknownToNumber, unknownToString } from "@/shared/utils/common/convert.util";
 import { createConverter, createMapper } from "@/shared/utils/common/mapper.util";
 
 export interface Location {
-    building   : string;
-    room       : string;
+    id         : number;
+    building_id: number;
+    name       : string;
     description: string;
+    capacity   : number;
 }
 
 export const mapLocation = createMapper<Location>({
-    building   : createConverter(unknownToString, ""),
-    room       : createConverter(unknownToString, ""),
+    id         : createConverter(unknownToNumber, 0),
+    building_id: createConverter(unknownToNumber, 0), // eslint-disable-line
+    name       : createConverter(unknownToString, ""),
+    description: createConverter(unknownToString, ""),
+    capacity   : createConverter(unknownToNumber, 0),
+});
+
+export interface Building {
+    id         : number;
+    name       : string;
+    description: string;
+}
+
+export const mapBuilding = createMapper<Building>({
+    id         : createConverter(unknownToNumber, 0),
+    name       : createConverter(unknownToString, ""),
     description: createConverter(unknownToString, ""),
 });
