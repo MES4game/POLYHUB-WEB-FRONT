102ccec19e214a13f0c66fbe91475d7de1eaf689
JulienTAP <tap.julien91@gmail.com>
2025-11-12 21:51:17 +0100

    fix(dev-events): fix last merge conflict

--- src/shared/models/common/lesson.model.ts
+++ src/shared/models/common/lesson.model.ts
@@ -28,17 +28,3 @@ export const mapLessonGroup = createMapper<LessonGroup>({
     lesson_type_id: createConverter(unknownToNumber, 0), // eslint-disable-line @typescript-eslint/naming-convention
     lesson_args   : createConverter(unknownToNumber, 0), // eslint-disable-line @typescript-eslint/naming-convention
 });
-
-export interface LessonGroup {
-    group_id      : number;
-    lesson_args   : number;
-    lesson_type_id: number;
-    lesson_id     : number;
-}
-
-export const mapLessonGroup = createMapper<LessonGroup>({
-    group_id      : createConverter(unknownToNumber, 0), // eslint-disable-line @typescript-eslint/naming-convention
-    lesson_id     : createConverter(unknownToNumber, 0), // eslint-disable-line @typescript-eslint/naming-convention
-    lesson_type_id: createConverter(unknownToNumber, 0), // eslint-disable-line @typescript-eslint/naming-convention
-    lesson_args   : createConverter(unknownToNumber, 0), // eslint-disable-line @typescript-eslint/naming-convention
-});
