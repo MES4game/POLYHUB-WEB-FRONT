aa1dab4f1f0849f47a64fec2f6becc9dbda9dd26
MHoupli <maelhoupline@gmail.com>
2025-11-12 11:37:17 +0100

    fix(SCRUM-22-AdminPage): fix lint

--- src/ui/components/home/navbar/navbar.component.tsx
+++ src/ui/components/home/navbar/navbar.component.tsx
@@ -64,14 +64,22 @@ const NavbarComp: FC<NavbarCompProps> = ({ calendarformat, onFormatChange }): Re
                                     <li><Link to="/register">Inscription</Link></li>
                                 </>
                             )}
+
                             {token.current && (
                                 <>
-                                    <li><Link to="/login" onClick={() => {
-                                        handleDisconnect();
-                                    }}>Se déconnecter</Link></li>
-                                    {(is_admin.current || is_modo.current) && (
-                                        <li><Link to="/admin">Page administrateur</Link></li>
-                                    )}
+                                    <li>
+                                        <Link
+                                            to="/login"
+                                            onClick={() => {
+                                                handleDisconnect();
+                                            }}
+                                        >
+                                            Se déconnecter
+                                        </Link>
+                                    </li>
+
+                                    {(is_admin.current || is_modo.current)
+                                        && <li><Link to="/admin">Page administrateur</Link></li>}
                                 </>
                             )}
                         </ul>
