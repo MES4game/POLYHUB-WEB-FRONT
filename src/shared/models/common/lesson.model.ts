df97365ec5b3c75d3346f821b389e3afb96e7022
JulienTAP <tap.julien91@gmail.com>
2025-11-12 16:01:19 +0100

    feat(dev-events): add possibility to post event

--- src/shared/models/common/lesson.model.ts
+++ src/shared/models/common/lesson.model.ts
@@ -30,12 +30,14 @@ export const mapLessonGroup = createMapper<LessonGroup>({
 });
 
 export interface LessonGroup {
+    group_id      : number;
     lesson_args   : number;
     lesson_type_id: number;
     lesson_id     : number;
 }
 
 export const mapLessonGroup = createMapper<LessonGroup>({
+    group_id      : createConverter(unknownToNumber, 0), // eslint-disable-line @typescript-eslint/naming-convention
     lesson_id     : createConverter(unknownToNumber, 0), // eslint-disable-line @typescript-eslint/naming-convention
     lesson_type_id: createConverter(unknownToNumber, 0), // eslint-disable-line @typescript-eslint/naming-convention
     lesson_args   : createConverter(unknownToNumber, 0), // eslint-disable-line @typescript-eslint/naming-convention
