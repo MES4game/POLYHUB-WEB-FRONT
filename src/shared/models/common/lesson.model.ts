20217fbe8e14fe0501a03dfb07623781dafba8cd
JulienTAP <tap.julien91@gmail.com>
2025-11-12 06:51:06 +0100

    feat(dev-events): add Promo,Group, lessonType,rooms fetch from backend; cours and teachers are not working properly for now

--- src/shared/models/common/lesson.model.ts
+++ src/shared/models/common/lesson.model.ts
@@ -28,3 +28,15 @@ export const mapLessonGroup = createMapper<LessonGroup>({
     lesson_type_id: createConverter(unknownToNumber, 0), // eslint-disable-line @typescript-eslint/naming-convention
     lesson_args   : createConverter(unknownToNumber, 0), // eslint-disable-line @typescript-eslint/naming-convention
 });
+
+export interface LessonGroup {
+    lesson_args   : number;
+    lesson_type_id: number;
+    lesson_id     : number;
+}
+
+export const mapLessonGroup = createMapper<LessonGroup>({
+    lesson_id     : createConverter(unknownToNumber, 0), // eslint-disable-line @typescript-eslint/naming-convention
+    lesson_type_id: createConverter(unknownToNumber, 0), // eslint-disable-line @typescript-eslint/naming-convention
+    lesson_args   : createConverter(unknownToNumber, 0), // eslint-disable-line @typescript-eslint/naming-convention
+});
