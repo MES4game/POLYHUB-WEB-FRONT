650b9c59ba0a6119f750111812e2d2f4991a96a0
JulienTAP <tap.julien91@gmail.com>
2025-11-13 14:22:50 +0100

    fix(dev-fix): fix merging duplicates

--- src/ui/components/admin/edt/eventButton.component.tsx
+++ src/ui/components/admin/edt/eventButton.component.tsx
@@ -16,7 +16,7 @@ const EventButtonComp: FC = (): ReactNode => {
                     className="new-event-button w-full"
                     onClick={() => { setIsDialogOpen(true); }}
                 >
-                    Nouveau Cour
+                    Ajouter un cours
                     <Plus className="h-4 w-4" />
                 </Button>
 
